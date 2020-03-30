9d2945846298401d269cb6072183f921a7daf203
Rik Thomas <rik.thomas@mediconesystems.com>
2020-03-30 23:00:58 +0100

    Mmm....

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,63 @@ class HomeController extends Controller
 {
     public function index()
     {
+        $readings = $this->readings();
+
+        $dates = [];
+        $j_results = [];
+        $r_results = [];
+        $temp_j_results = [];
+        $temp_r_results = [];
+
+        $current_date = false;
+
+        foreach ($readings as $reading) {
+            $date = Carbon::parse($reading->time)->format('d/m/y');
+            if (!in_array($date, $dates)) {
+                array_push($dates, $date);
+            }
+            if (!$current_date) {
+                array_push($temp_j_results, $reading->eng_flow);
+                array_push($temp_r_results, $reading->real_flow);
+                $current_date = $date;
+            } elseif ($current_date === $date) {
+                array_push($temp_j_results, $reading->eng_flow);
+                array_push($temp_r_results, $reading->real_flow);
+            } elseif ($current_date != $date) {
+                array_push($j_results, $temp_j_results);
+                array_push($r_results, $temp_r_results);
+                $temp_j_results = [];
+                $temp_r_results = [];
+                $current_date = $date;
+                array_push($temp_j_results, $reading->eng_flow);
+                array_push($temp_r_results, $reading->real_flow);
+            }
+        }
+
+        if (!empty($temp_r_results)) {
+            array_push($j_results, $temp_j_results);
+            array_push($r_results, $temp_r_results);
+            $temp_j_results = [];
+            $temp_r_results = [];
+        }
+
+        $avg_j_result = [];
+        $avg_r_result = [];
+
+        foreach ($j_results as $j_result) {
+            array_push($avg_j_result, round(Stats::mean($j_result)));
+        }
+
+        foreach ($r_results as $r_result) {
+            array_push($avg_r_result, round(Stats::mean($r_result)));
+        }
+
+        JavaScript::put([
+            'dates' => array_reverse($dates),
+            'avg_j_result' => array_reverse($avg_j_result),
+            'avg_r_result' => array_reverse($avg_r_result)
+        ]);
+
         return view('home');
     }
 
@@ -49,6 +106,19 @@ class HomeController extends Controller
     }
 
     public function data()
+    {
+        $readings = $this->readings();
+
+        $data = [];
+
+        foreach ($readings as $reading) {
+            array_push($data, [strtotime($reading->time) * 1000, (float) $reading->real_flow]);
+        }
+
+        return json_encode(array_reverse($data));
+    }
+
+    public static function readings()
     {
         $readings = DB::select(
             "SELECT 
@@ -94,14 +164,8 @@ class HomeController extends Controller
                     tank_b) b4 ON b4.id = b3.id + 1 "
         );
 
-        // return $readings;
-
-        $data = [];
-
-        foreach ($readings as $reading) {
-            array_push($data, [strtotime($reading->time) * 1000, (float) $reading->real_flow]);
-        }
-
-        return json_encode(array_reverse($data));
+        return array_filter($readings, function ($item) {
+            return $item->minutes >= 60;
+        });
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,8 @@
             </div>
             <br><br>
             <div id="container" style="width:100%; height:400px;"></div>
+            <br><br>
+            <div id="container_time" style="width:100%; height:400px;"></div>
         </div>
     </div>
 </div>
@@ -18,16 +20,63 @@
 
 @push('scripts')
 <script>
+    Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Daily Average Flowrate'
+    },
+    xAxis: {
+        categories: dates,
+        crosshair: true,
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Flow L/min'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f}L/min</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        },
+        series: {
+            dataLabels: {
+                enabled: false
+            }
+        }
+    },
+    credits: {
+        enabled: false,
+    },
+    series: [{
+        data: avg_r_result,
+        name: 'Average Flow',
+        showInLegend: false
+    }],
+});
+
+
     $.getJSON(
-    'http://homestead.test/data',
+    '/data',
     function (data) {
 
-        Highcharts.chart('container', {
+        Highcharts.chart('container_time', {
             chart: {
                 zoomType: 'x'
             },
             title: {
-                text: 'Flowrate through VIE regulator'
+                text: 'Regulator Flowrate'
             },
             subtitle: {
                 text: document.ontouchstart === undefined ?
@@ -37,8 +86,9 @@
                 type: 'datetime'
             },
             yAxis: {
+                min: 0,
                 title: {
-                    text: 'Flow L / min'
+                    text: 'Flow L/min'
                 }
             },
             legend: {
@@ -70,10 +120,12 @@
                     threshold: null
                 }
             },
-
+            credits: {
+                enabled: false,
+            },
             series: [{
                 type: 'area',
-                name: 'Flow L / min',
+                name: 'Flow L/min',
                 data: data
             }]
         });
