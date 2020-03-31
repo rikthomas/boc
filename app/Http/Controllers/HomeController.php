f653c9b4baa5ac84e54cfe8a98d096b56788a3c6
Mark Salmon <marksalmon@mark.salmon@mediconesystems.com>
2020-03-31 18:39:49 +0100

    clean

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,59 +15,26 @@ class HomeController extends Controller
     {
         $readings = $this->readings();
 
-        $dates = [];
-        $j_results = [];
-        $r_results = [];
-        $temp_j_results = [];
-        $temp_r_results = [];
+        $dates = $readings->map(function ($reading) {
+            return Carbon::parse($reading->time)->format('d/m/y');
+        })->unique()->values()->toArray();
 
-        $current_date = false;
+        $avg_j_result = $readings->groupBy(function ($reading) {
+            return Carbon::parse($reading->time)->format('d/m/y');
+        })->map(function ($group) {
+            return round($group->avg('eng_flow'));
+        })->reverse()->values()->toArray();
 
-        foreach ($readings as $reading) {
-            $date = Carbon::parse($reading->time)->format('d/m/y');
-            if (!in_array($date, $dates)) {
-                array_push($dates, $date);
-            }
-            if (!$current_date) {
-                array_push($temp_j_results, $reading->eng_flow);
-                array_push($temp_r_results, $reading->real_flow);
-                $current_date = $date;
-            } elseif ($current_date === $date) {
-                array_push($temp_j_results, $reading->eng_flow);
-                array_push($temp_r_results, $reading->real_flow);
-            } elseif ($current_date != $date) {
-                array_push($j_results, $temp_j_results);
-                array_push($r_results, $temp_r_results);
-                $temp_j_results = [];
-                $temp_r_results = [];
-                $current_date = $date;
-                array_push($temp_j_results, $reading->eng_flow);
-                array_push($temp_r_results, $reading->real_flow);
-            }
-        }
-
-        if (!empty($temp_r_results)) {
-            array_push($j_results, $temp_j_results);
-            array_push($r_results, $temp_r_results);
-            $temp_j_results = [];
-            $temp_r_results = [];
-        }
-
-        $avg_j_result = [];
-        $avg_r_result = [];
-
-        foreach ($j_results as $j_result) {
-            array_push($avg_j_result, round(Stats::mean($j_result)));
-        }
-
-        foreach ($r_results as $r_result) {
-            array_push($avg_r_result, round(Stats::mean($r_result)));
-        }
+        $avg_r_result = $readings->groupBy(function ($reading) {
+            return Carbon::parse($reading->time)->format('d/m/y');
+        })->map(function ($group) {
+            return round($group->avg('real_flow'));
+        })->reverse()->values()->toArray();
 
         JavaScript::put([
-            'dates' => array_reverse($dates),
-            'avg_j_result' => array_reverse($avg_j_result),
-            'avg_r_result' => array_reverse($avg_r_result)
+            'dates' => $dates,
+            'avg_j_result' => $avg_j_result,
+            'avg_r_result' => $avg_r_result
         ]);
 
         return view('home', compact('readings'));
